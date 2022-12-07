7e8f40e32b95a46590656df980e06ec9356a9bc0
zfaridr <zainullovfr@gmail.com>
2022-12-07 17:59:32 +0300

    lesson_8 Tests

--- config/settings.py
+++ config/settings.py
@@ -230,3 +230,6 @@ EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = "var/email-messages/"
 
 LOCALE_PATHS = [BASE_DIR / "locale"]
+
+
+SELENIUM_DRIVER_PATH_FF = BASE_DIR / "var" / "selenium" / "geckodriver"
